07768862ecc0f8626a1f8859dd660e8641be6a73
Ramonglez1 <141690356+Ramonglez1@users.noreply.github.com>
2023-08-16 12:59:18 -0600

    Update sarcopenia.py

--- sarcopenia.py
+++ sarcopenia.py
@@ -53,7 +53,8 @@ st.markdown(
 
 col1, col2, col3 = st.columns(3)
 with col1:
-        st.write("Tipos de sarcopenia")
+        st.Header("Tipos de sarcopenia")
+        image = Image.open("Tipos-de-sarcopenia.jpg")
 with col2:
         st.write(" ")
 with col3:
