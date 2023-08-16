39f0037a7a91144b9b295aa3cb5d00b5e63f0a06
Ramonglez1 <141690356+Ramonglez1@users.noreply.github.com>
2023-08-16 13:53:56 -0600

    Update sarcopenia.py

--- sarcopenia.py
+++ sarcopenia.py
@@ -74,12 +74,15 @@ with tab1:
     st.header("Fuerza de brazo")
     st.markdown(
             """
-       eeeeeee
+       Las mediciones se realizan con un dinamómetro hidráulico manual, el cual se utiliza para medir la fuerza de agarre. Es utilizado como herramienta diagnóstica y evolución de un paciente, en este caso como criterio para el diagnóstico de sarcopenia.
+
+La medición de la fuerza de agarre máxima (MGS) es un elemento esencial para evaluar a la fuerza durante el crecimiento, envejecimiento, en las secuelas de lesiones traumáticas y en la rehabilitación. El dinamómetro es considerado un instrumento adecuado y confiable para la evaluación de la fuerza de prensión manual del paciente, aunque la fiabilidad de la evaluación puede verse afectada por género, peso y la postura corporal.
+
             """
     )    
 col1, col2, col3 = st.columns(3)
 with col1:
-        st.write("Tipos de sarcopenia")
+        st.write(" ")
 with col2:
         st.write(" ")
 with col3:
