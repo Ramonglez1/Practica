42427c97f7563fd22274c14c65ea9b82c2772fd2
Ramonglez1 <141690356+Ramonglez1@users.noreply.github.com>
2023-08-18 12:51:07 -0600

    Update sarcopenia.py

--- sarcopenia.py
+++ sarcopenia.py
@@ -56,8 +56,8 @@ col1, col2, col3 = st.columns(3)
 with col1:
         st.write(" ")
         image1 = "Tipos-de-sarcopenia.jpg"
-        st.image(image1)
         width = 800
+        st.image(image1,width = 800)
         st.caption(' :blue[Imagen 1: Tipos de Sarcopenia]')
 with col2:
         st.write(" ")
